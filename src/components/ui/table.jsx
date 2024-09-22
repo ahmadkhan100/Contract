2fd7e3ed87c8f7241451a7dd44f25d39cded01ee
Ahmad Khan <163046609+ahmadkhan100@users.noreply.github.com>
2024-09-22 18:29:47 -0400

    Create table.jsx

---
+++ src/components/ui/table.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+export const Table = ({ children, className = '', ...props }) => (
+  <table className={`min-w-full divide-y divide-gray-200 ${className}`} {...props}>
+    {children}
+  </table>
+);
+
+export const TableHeader = ({ children, className = '', ...props }) => (
+  <thead className={`bg-gray-50 ${className}`} {...props}>
+    {children}
+  </thead>
+);
+
+export const TableBody = ({ children, className = '', ...props }) => (
+  <tbody className={`bg-white divide-y divide-gray-200 ${className}`} {...props}>
+    {children}
+  </tbody>
+);
+
+export const TableRow = ({ children, className = '', ...props }) => (
+  <tr className={className} {...props}>
+    {children}
+  </tr>
+);
+
+export const TableHead = ({ children, className = '', ...props }) => (
+  <th
+    className={`px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider ${className}`}
+    {...props}
+  >
+    {children}
+  </th>
+);
+
+export const TableCell = ({ children, className = '', ...props }) => (
+  <td className={`px-6 py-4 whitespace-nowrap ${className}`} {...props}>
+    {children}
+  </td>
+);
